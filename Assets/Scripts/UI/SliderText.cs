aeafbfdfe7aa71ca459039b000642b8ef7b868f4
Kilreth <jonathan.pavat.brown@gmail.com>
2020-06-07 17:43:02 +1200

    Update slider text upon its value changing

---
+++ Assets/Scripts/UI/SliderText.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+    [SerializeField]
+    private Text text;
+
+    public void UpdateText()
+    {
+        if (slider.wholeNumbers)
+        {
+            text.text = slider.value.ToString();
+        }
+        else
+        {
+            text.text = String.Format("{0:P0}", slider.value).Replace(" %", "%");
+        }
+    }
+}
