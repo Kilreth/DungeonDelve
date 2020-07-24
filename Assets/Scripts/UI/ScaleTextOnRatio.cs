873c49ebbfd05a2a213874d1d36dccdc687881ea
Kilreth <jonathan.pavat.brown@gmail.com>
2020-07-25 10:05:23 +1200

    Make text scaler script to support all aspect ratios, and apply it to one object

---
+++ Assets/Scripts/UI/ScaleTextOnRatio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScaleTextOnRatio : MonoBehaviour
+{
+    public TextMeshProUGUI TextPro;
+    public Text Text;
+    public float BaseRatio;
+    public float DefaultSize;
+    public float Power = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int fontSize = (int)(DefaultSize * Math.Pow(BaseRatio / Screen.width * Screen.height, Power));
+
+        if (TextPro != null)
+            TextPro.fontSize = fontSize;
+        if (Text != null)
+            Text.fontSize = fontSize;
+    }
+}
