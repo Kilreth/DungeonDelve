fe9b6d298a16ee7374ead9ff4d9984617a66a4ab
Kilreth <jonathan.pavat.brown@gmail.com>
2020-09-12 08:39:26 +1200

    Use buttons for the GitHub and SoundCloud links, making them highlightable

--- Assets/Scripts/UI/OpenHyperlinks.cs
+++ Assets/Scripts/UI/OpenHyperlinks.cs
@@ -1,27 +1,9 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
-using TMPro;
 
-/// <summary>
-/// Adapted from https://medium.com/@immops/make-clickable-links-in-unity-textmeshpro-8b72b24a820f
-/// </summary>
-[RequireComponent(typeof(TextMeshProUGUI))]
-public class OpenHyperlinks : MonoBehaviour, IPointerClickHandler
+public class OpenHyperlinks : MonoBehaviour
 {
-    private TextMeshProUGUI text;
-
-    void Awake()
+    public void OpenURL(string url)
     {
-        text = GetComponent<TextMeshProUGUI>();
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        int linkIndex = TMP_TextUtilities.FindIntersectingLink(text, Input.mousePosition, null);
-        if (linkIndex != -1)
-        {
-            TMP_LinkInfo linkInfo = text.textInfo.linkInfo[linkIndex];
-            Application.OpenURL(linkInfo.GetLinkID());
-        }
+        Application.OpenURL(url);
     }
 }
